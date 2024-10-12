de5dbcb805b95a1f099a0bf29dbddd37b9e31844
Fomagran <fomagran6@gmail.com>
2024-10-12 23:02:55 +0900

    Initial Commit

---
+++ Tests/OneWayKitTests/OneWayKitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import OneWayKit
+
+final class OneWayKitTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
