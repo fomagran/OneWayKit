780feae2dd15949f8976bf753c09b6389b9c88c3
Fomagran <fomagran@icloud.com>
2024-11-25 23:15:46 +0900

    feat: Add statePublisher and state (subject) in GlobalOneWay

--- Sources/OneWayKit/GlobalOneWay.swift
+++ Sources/OneWayKit/GlobalOneWay.swift
@@ -11,13 +11,22 @@ public final class GlobalOneWay: NSObject {
 
     private static var globalOneWays: [String: any OneWayHandlable] = [:]
     
-    public static func state<Feature: Featurable>(feature: Feature.Type) -> AnyPublisher<Feature.State, Never> {
+    public static func statePublisher<Feature: Featurable>(feature: Feature.Type) -> AnyPublisher<Feature.State, Never> {
         guard let oneWay = globalOneWays[Feature.id] as? OneWay<Feature> else {
             print("The initial state for the GlobalType \(feature) has not been registered.")
             return Empty().eraseToAnyPublisher()
         }
         
         return oneWay.subject.eraseToAnyPublisher()
+    }
+    
+    public static func state<Feature: Featurable>(feature: Feature.Type) -> CurrentValueSubject<Feature.State, Never>? {
+        guard let oneWay = globalOneWays[Feature.id] as? OneWay<Feature> else {
+            print("The initial state for the GlobalType \(feature) has not been registered.")
+            return nil
+        }
+        
+        return oneWay.subject
     }
     
     public static func send<Feature: Featurable>(feature: Feature.Type, _ action: Feature.Action) {
